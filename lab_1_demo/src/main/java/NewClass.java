6b552fae772b9d79ea8a3d1222a97d494b189dec
cgonc <cgonc@137.125.149.182>
2022-09-01 10:28:03 -0400

    sayhello added

--- lab_1_demo/src/main/java/NewClass.java
+++ lab_1_demo/src/main/java/NewClass.java
@@ -1,16 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author cgonc
- */
 public class NewClass {
     
+    private static void sayhello(String str){
+    
+        System.out.print(str);
+        
+} 
+    
     public static void main(String[] args) {
+      
+        sayhello("hello world");
         
     }
     

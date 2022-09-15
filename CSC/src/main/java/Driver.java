6defa290d8266407a8900ab0c187503a4b423561
cgonc <cgonc@137.125.149.182>
2022-09-15 09:51:41 -0400

    Student

--- CSC/src/main/java/Driver.java
+++ CSC/src/main/java/Driver.java
@@ -6,7 +6,9 @@ public class Driver {
     
     public static void main(String[] args) {
         
+        Student std = new Student();
         
+        System.out.println("The student is " + std.tostring());
         
     }
     
